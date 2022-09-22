39dbff9e4241f87679eb160f5110287315946254
kawamuragen <genn.kawamura@gmail.com>
2022-09-22 23:48:13 +0900

    Implemented logic to add items to the cart. Up to lesson 61.

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ class MyApp extends App {
   // const [state, setState] = useState(null);
   state = {
     user: null,
+    cart: { items: [], total: 0 },
   };
 
   setUser = (user) => {
@@ -39,11 +40,57 @@ class MyApp extends App {
     }
   }
 
+  // カートへ商品の追加
+  addItem = (item) => {
+    let { items } = this.state.cart;
+    const newItem = items.find((i) => i.id === item.id);
+    console.log("_app.js addItem(item) newItem:");
+    console.log(newItem);
+
+    // 新しい商品追加の場合
+    if (!newItem) {
+      item.quantity = 1;
+      // cartに追加する
+      this.setState(
+        {
+          cart: {
+            items: [...items, item],
+            total: this.state.cart.total + item.price,
+          },
+        },
+        () => Cookies.set("cart", this.state.cart.items)
+      );
+    }
+    // すでにカートに商品入ってる場合
+    else {
+      this.setState(
+        {
+          cart: {
+            items: this.state.cart.items.map((item) =>
+              // itemオブジェクトに対してquantityフィールドを追加する
+              // Object.assign 既存の配列に対して何かフィールドを追加したいときなど
+              item.id === newItem.id
+                ? Object.assign({}, item, { quantity: item.quantity + 1 })
+                : item
+            ),
+            total: this.state.cart.total + item.price,
+          },
+        },
+        () => Cookies.set("cart", this.state.cart.items)
+      );
+    }
+  };
+
   render() {
     const { Component, pageProps } = this.props;
     return (
       <AppContext.Provider
-        value={{ user: this.state.user, setUser: this.setUser }}
+        value={{
+          user: this.state.user,
+          cart: this.state.cart,
+          setUser: this.setUser,
+          addItem: this.addItem,
+        }}
       >
         <>
           <Head>
--- pages/restaurants.js
+++ pages/restaurants.js
@@ -8,8 +8,11 @@ import {
   Button,
 } from "reactstrap";
 import { gql } from "apollo-boost";
+import { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { useRouter } from "next/router";
+
+import AppContext from "../context/AppContext";
 import Cart from "../components/Cart";
 
 // どのレストラン情報を取るのか決めるために$idとする
@@ -34,6 +37,8 @@ const GET_RESTAURANT_DISHES = gql`
 
 // nafe
 const Restaurants = (props) => {
+  const appContext = useContext(AppContext);
+
   const router = useRouter();
 
   const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, {
@@ -54,20 +59,24 @@ const Restaurants = (props) => {
       <>
         <h1>{restaurant.name}</h1>
         <Row>
-          {restaurant.dishes.map((restaurant) => (
-            <Col xs="6" sm="4" key={restaurant.id} style={{ padding: 0 }}>
+          {restaurant.dishes.map((dish) => (
+            <Col xs="6" sm="4" key={dish.id} style={{ padding: 0 }}>
               <Card style={{ margin: "0 10px" }}>
                 <CardImg
-                  src={`${process.env.NEXT_PUBLIC_API_URL}${restaurant.image.url}`}
+                  src={`${process.env.NEXT_PUBLIC_API_URL}${dish.image.url}`}
                   top={true}
                   sytle={{ height: 250 }}
                 />
                 <CardBody>
-                  <CardTitle>{restaurant.name}</CardTitle>
-                  <CardTitle>{restaurant.description}</CardTitle>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardTitle>{dish.description}</CardTitle>
                 </CardBody>
                 <div className="card-footer">
-                  <Button outline color="primary">
+                  <Button
+                    outline
+                    color="primary"
+                    onClick={() => appContext.addItem(dish)}
+                  >
                     + カートに入れる
                   </Button>
                 </div>

41287613a45ce8ccadbb6f87c30b3558e2bab5a6
kawamuragen <genn.kawamura@gmail.com>
2022-10-02 10:21:50 +0900

    Create a credit card form using the Stripe library. Up to lesson 75.

--- components/Checkout/CardSection.js
+++ components/Checkout/CardSection.js
@@ -1,5 +1,38 @@
+import { CardElement } from "@stripe/react-stripe-js";
+
 const CardSection = () => {
-  return <div>Enter</div>;
+  return (
+    <div>
+      <div>
+        <label htmlFor="card-element">クレジット／デビットカード</label>
+
+        <div>
+          <fieldset>
+            <div className="form-row">
+              <div id="card-element" style={{ width: "100%" }}>
+                {/* Stripeが用意してくれるCardコンポーネント */}
+                <CardElement />
+              </div>
+              <br />
+              <div className="order-button-wrapper">
+                <button>注文を確認</button>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .order-button-wrapper {
+            display: flex;
+            width: 100%;
+            align-items: flex-end;
+            justify-content: flex-end;
+          }
+        `}
+      </style>
+    </div>
+  );
 };
 
 export default CardSection;
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,9 +1,18 @@
 import { Col, Row } from "reactstrap";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 import Cart from "../components/Cart";
 import CheckOutForm from "../components/Checkout/CheckOutForm";
 
 const checkout = () => {
+  // loadStripe @stripe/stripe-js が必要
+  // 公開キーを設定する
+  // <Elements>にstripeのpropsとして渡す
+  const stripePromise = loadStripe(
+    "pk_test_51LlOBTFuj5CHTwb2fx4HoVhhkQz5C8oNxLheBB6oTxSHu9u4BFoSU8u4qHr8aK1zkDmKS2q1XpAd5gw16lcCbtTn00G9bm8Rv5"
+  );
+
   return (
     <Row>
       <Col style={{ paddingRight: 0 }} sm={{ size: 3, order: 1, offset: 2 }}>
@@ -13,7 +22,9 @@ const checkout = () => {
         <Cart />
       </Col>
       <Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
-        <CheckOutForm />
+        <Elements stripe={stripePromise}>
+          <CheckOutForm />
+        </Elements>
       </Col>
     </Row>
   );
